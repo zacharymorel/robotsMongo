eac668ca00592e521fe28d8a9e34bbd28c2d162f
Zachary Morel <morelz133@gmail.com>
2017-08-15 11:15:35 -0400

    Now have bugs fixed and added specific find for unemployeed robots

--- server.js
+++ server.js
@@ -1,36 +1,35 @@
 const express = require('express')
 const mustacheExpress = require('mustache-express')
-const MongoClient = require('mongodb').MongoClient, assert = require('assert')
+const MongoClient = require('mongodb').MongoClient
+const assert = require('assert')
 const bodyParser = require('body-parser')
 const app = express()
 let database
 
 var url = 'mongodb://localhost:27017/robots'
 
+app.engine('mustache', mustacheExpress())
 
-app.engine("mustache", mustacheExpress())
-
-app.set("views", "./views")
-app.set("view engine", "mustache")
-app.use(express.static("public"))
+app.set('views', './views')
+app.set('view engine', 'mustache')
+app.use(express.static('public'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
-
-const getAllRobots = (db, callback) => {
-  let collection = db.collection("robots")
-  collection.find({}).toArray((err, docs) => {
-    // console.log({ err: docs })
-    callback(err, docs)
-  })
+const getAllRobots = callback => {
+  let collection = database.collection('robots')
+  // console.log(collection)
+  collection.find({}).toArray(callback)
 }
 
-const getOneRobot = (db, id, callback) => {
-  let collection = db.collection("robots")
-  collection.findOne({id: id}) => {
-    callback(err, robot)
-    // Do I tell it to query for _id? And how does that match up to the url id?
-  }
+const getOneRobot = (id, callback) => {
+  let collection = database.collection('robots')
+  // console.log(id)
+  collection.findOne({ id: id }, callback)
+}
+const getAllAvailbleHire = callback => {
+  let collection = database.collection('robots')
+  collection.find({ job: null }).toArray(callback)
 }
 
 MongoClient.connect(url, (err, db) => {
@@ -38,26 +37,33 @@ MongoClient.connect(url, (err, db) => {
   database = db
   console.log('Successfully connected with mongo server.')
 
-    getAllRobots(db, (err, robots) => {
-      console.log("success")
-      // console.log({err, robots});
-    })
+  getAllRobots((err, robots) => {
+    console.log('success')
+    // console.log({err, robots});
+  })
 })
 
-
 app.get('/', (request, response) => {
-  getAllRobots(database, (err, robots) => {
-    response.render('home', {robots: robots})
+  getAllRobots((err, robots) => {
+    response.render('home', { robots: robots })
   })
 })
 
 app.get('/robots/:id', (request, response) => {
   let id = parseInt(request.params.id)
-  getOneRobot(database, id, (err, robot) => {
-    response.render('personal', {robot: robot})
+  getOneRobot(id, (err, robot) => {
+    // console.log(robot)
+    response.render('personal', robot)
+    // Ask about this "robot" not an object, lately
+  })
+})
+
+app.get('/availableforhire', (request, response) => {
+  getAllAvailbleHire((err, robots) => {
+    response.render('availableforhire', { robots: robots })
   })
 })
 
 app.listen(3000, () => {
-  console.log('May the force be with you');
+  console.log('May the force be with you')
 })
